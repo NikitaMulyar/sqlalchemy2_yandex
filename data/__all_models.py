8ee73f446a1779103586cd14706fce6d26021d1a
NikitaMulyar <ni***ol@mail.ru>
2023-02-23 01:03:23 +0300

    Вся классная

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,2 +1,5 @@
 from . import users
 from . import news
+from . import category
+from . import jobs
+from . import departments
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from data import db_session
 from data.users import User
 from data.news import News
 from data.jobs import Jobs
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, abort
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField, BooleanField, StringField, IntegerField, DateTimeField
+from wtforms import EmailField, PasswordField, SubmitField, BooleanField, StringField, IntegerField, DateTimeField, TextAreaField
 from wtforms.validators import DataRequired
 
 
@@ -38,6 +38,13 @@ class JobForm(FlaskForm):
     end_date = DateTimeField('End date', default=None)
     done = BooleanField('Is finished?')
     submit = SubmitField('Add job')
+
+
+class NewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    is_private = BooleanField("Личное")
+    submit = SubmitField('Применить')
 
 
 app = Flask(__name__)
@@ -147,6 +154,75 @@ def addjob():
                                message="Неправильный адрес почты тимлида",
                                form=form)
     return render_template('job_add.html', title='Adding a Job', form=form)
+
+
+@app.route('/news',  methods=['GET', 'POST'])
+@login_required
+def add_news():
+    form = NewsForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = News()
+        news.title = form.title.data
+        news.content = form.content.data
+        news.is_private = form.is_private.data
+        res = db_sess.query(User).filter(User.id == current_user.id).first()
+        news.user_id = res.id
+        # current_user.news.append(news)
+        db_sess.add(news)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('news.html', title='Добавление новости',
+                           form=form)
+
+
+@app.route('/news/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_news(id):
+    form = NewsForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            form.title.data = news.title
+            form.content.data = news.content
+            form.is_private.data = news.is_private
+        else:
+            abort(404)
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        news = db_sess.query(News).filter(News.id == id,
+                                          News.user == current_user
+                                          ).first()
+        if news:
+            news.title = form.title.data
+            news.content = form.content.data
+            news.is_private = form.is_private.data
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('news.html',
+                           title='Редактирование новости',
+                           form=form
+                           )
+
+
+@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def news_delete(id):
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.id == id,
+                                      News.user == current_user
+                                      ).first()
+    if news:
+        db_sess.delete(news)
+        db_sess.commit()
+    else:
+        abort(404)
+    return redirect('/')
 
 
 if __name__ == '__main__':
